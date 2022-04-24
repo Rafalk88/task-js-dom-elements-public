2a252525bd2d70f89a804d5858fde3317f9e8d59
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-24 13:03:20 +0200

    problem 1 solved

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -1,4 +1,35 @@
 const tooltipElements = document.querySelectorAll('.tooltip')
 
-const aElement = document.createElement('a')
-const spanElement = document.createElement('span')
+tooltipElements.forEach(function(span) {
+
+    const aElement = document.createElement('a')
+    const spanElement = document.createElement('span')
+
+    // ----------------- a attributes created ------------------------
+    aElement.innerHTML = span.innerText
+    aElement.href = span.dataset.url
+    span.innerHTML = ''
+    // --------------------------------------------------------------
+
+    // ----------------- span attributes created ---------------------
+    if (span.dataset.tooltipType === 'image') {
+
+        spanElement.classList.add('tooltip__box', 'tooltip__box--image')
+
+        const imgElement = document.createElement('img')
+        imgElement.classList.add('tooltip__image')
+        imgElement.src = span.dataset.tooltipContent
+        spanElement.appendChild(imgElement)
+
+    } else {
+
+        spanElement.classList.add('tooltip__box', 'tooltip__box--text')
+        spanElement.innerHTML = span.dataset.tooltipContent
+
+    }
+    // --------------------------------------------------------------
+
+    span.appendChild(aElement)
+    span.appendChild(spanElement)
+
+})
